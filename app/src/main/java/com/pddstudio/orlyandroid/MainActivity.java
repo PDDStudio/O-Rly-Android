266c48d66975be068ac2e0bd84185fd7ea8ac514
Patrick Jung <patrick.jung@arconsis.com>
2016-08-14 22:54:04 +0200

    Adding android-annotations to app module

--- app/src/main/java/com/pddstudio/orlyandroid/MainActivity.java
+++ app/src/main/java/com/pddstudio/orlyandroid/MainActivity.java
@@ -1,13 +1,10 @@
 package com.pddstudio.orlyandroid;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+	
 }
